6fb8ecc2521900354552e296f4cf5eb16c5e67bf
thien1720 <thienpv@consultindochina.com>
2023-04-29 14:54:33 +0700

    home-page

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -110,12 +110,14 @@ function HomePage(options) {
         </div>
 
         <section className={cx("content")}>
-            <div className={cx("content-feature")}>
-                <p className={cx("feature-intro")}>"Nếu bạn đồng hành cùng Cơm Có Thịt, thì đó chỉ là do mệnh lệnh từ trái tim của bạn" - Trần Đăng Tuấn</p>
+            <div className="container">
+                <p className={cx("feature-intro")}>
+                    "Nếu bạn đồng hành cùng Cơm Có Thịt, thì đó chỉ là do mệnh lệnh từ trái tim của bạn" - Trần Đăng Tuấn
+                </p>
             </div>
 
             {/* transfer info */}
-            <div className={cx("transparency-info", "container")}>
+            <div className={cx("transparency-info")}>
                 <div className={cx("tranfer-info")}>
                     <h2 className={cx("title-tranfer")}>THỐNG KÊ ỦNG HỘ QUỸ TRÒ NGHÈO VÙNG CAO</h2>
                     <div className={cx("desc-info", "fund-info")}>
@@ -130,80 +132,85 @@ function HomePage(options) {
                         Ngân hàng Ngoại Thương Việt Nam - Vietcombank, chi nhánh Ba Đình, Hà Nội
                     </div>
                 </div>
+                <div className="container">
 
-                <div className={cx("row")}>
-                    <div className={cx("total-bank col-xs-12 col-md-6")}>
-                        <div className={cx("info-fund-banck")}>
-                            <p> Tài khoản Vietcombank</p>
-                            <p className={cx("name-fund")}>QUY TRO NGHEO VUNG CAO</p>
-                        </div>
-                        <div className={cx("form-total")}>
+                    <div className={cx("row")}>
+                        <div className={cx("total-bank col-12 col-md-6")}>
+                            <div className={cx("info-fund-banck")}>
+                                <p> Tài khoản Vietcombank</p>
+                                <p className={cx("name-fund")}>QUY TRO NGHEO VUNG CAO</p>
+                            </div>
+
                             <Box
                                 component="form"
                                 sx={{
-                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                    // '& .MuiTextField-root': { m: 1, width: '25ch' },
                                 }}
                                 noValidate
                                 autoComplete="off"
                             >
-                                <div>
-                                    <TextField
-                                        id="outlined-read-only-input"
-                                        label="Số tài khoản"
-                                        defaultValue="0611 0019 17137"
-                                        className={cx("style-input")}
-                                        InputProps={{
-                                            inputComponent: CustomInputComponent,
-                                            readOnly: true
-                                        }}
-                                        // variant="filled"
-                                        focused
-                                    />
-                                    <TextField
-                                        id="outlined-multiline-flexible"
-                                        label="Số tiền ủng hộ (VNĐ)"
-                                        className={cx("style-input")}
-                                    // value="Số tiền ủng hộ (VNĐ)"
-                                    // multiline
-                                    // maxRows={4}
-                                    />
-                                    <TextField
-                                        id="outlined-textarea"
-                                        className={cx("style-input")}
-                                        label="Nội dung chuyển khoản"
-                                    // placeholder="Placeholder"
-                                    // multiline
-                                    />
-                                </div>
+
+                                <TextField
+                                    id="outlined-read-only-input"
+                                    label="Số tài khoản"
+                                    defaultValue="0611 0019 17137"
+                                    className={cx("style-input")}
+                                    InputProps={{
+                                        inputComponent: CustomInputComponent,
+                                        readOnly: true
+                                    }}
+                                    // variant="filled"
+                                    focused
+                                />
+                                <TextField
+                                    id="outlined-multiline-flexible"
+                                    label="Số tiền ủng hộ (VNĐ)"
+                                    className={cx("style-input")}
+                                // value="Số tiền ủng hộ (VNĐ)"
+                                // multiline
+                                // maxRows={4}
+                                />
+                                <TextField
+                                    className={cx("style-input")}
+                                    id="outlined-textarea"
+                                    className={cx("style-input")}
+                                    label="Nội dung chuyển khoản"
+                                // placeholder="Placeholder"
+                                // multiline
+                                />
+
                             </Box>
+
+
+                        </div>
+                        <div className={cx("total-bank co-12 col-md-6")}>
+                            <div className={cx("Pay-QR")}>
+
+                                <div className={cx("title-QR")}>
+                                    Chuyển khoản <span>Viet</span> <span>QR</span>
+                                </div>
+
+                                <div className={cx("img-Qr")}>
+                                    <img src="" alt="image-QR" />
+                                </div>
+                                <div className={cx("sub-QR")}>
+                                    <img src="./image/logo_napas247.png" alt="27/7" />
+                                    <img src="./image/logo_vietcombank.png" alt="banck" />
+
+                                </div>
+                            </div>
+
                         </div>
 
                     </div>
-                    <div className={cx("total-bank col-xs-12 col-md-6")}>
-                        <div className={cx("Pay-QR")}>
-
-                            <div className={cx("title-QR")}>
-                                Chuyển khoản <span>Viet</span> <span>QR</span>
-                            </div>
-
-                            <div className={cx("img-Qr")}>
-                                <img src="" alt="image-QR" />
-                            </div>
-                            <div className={cx("sub-QR")}>
-                                <img src="./image/logo_napas247.png" alt="27/7" />
-                                <img src="./image/logo_vietcombank.png" alt="banck" />
-
-                            </div>
-                        </div>
-
-                    </div>
-
                 </div>
 
+
             </div>
-            <div className={cx("support-list", "container-xl")}>
+            <div className="container">
                 <div className={cx("head-list-sup")}>
                     <p>Powered by <a href="https://chuyenkhoan.com/">Chuyenkhoan.com</a>.</p>
+
 
                 </div>
                 <div className={cx("list-suport-total")}>
@@ -454,7 +461,7 @@ function HomePage(options) {
                         <table class="table">
                             <thead>
                                 <tr>
-                                    <th scope="col">Tài khoản nhận</th>
+                                    <th scope="col" className={cx("stk-info")} >Tài khoản nhận</th>
                                     <th scope="col">Ngày chuyển</th>
                                     <th scope="col">Số tiền</th>
                                     <th scope="col">Nội dung</th>
@@ -462,23 +469,19 @@ function HomePage(options) {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th scope="row">0611001917137</th>
+                                    <th scope="row" className={cx("stk-info")}>0611001917137</th>
                                     <td>1 phút trước
                                         2023-04-28 11:41:30</td>
                                     <td>+9.000</td>
                                     <td>837496.280423.120143.THACH TAN Chuyen tien</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">2</th>
+                                    <th scope="row" className={cx("stk-info")}>2</th>
                                     <td>Jacob</td>
                                     <td>Thornton</td>
                                     <td>@fat</td>
                                 </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
+
                             </tbody>
                         </table>
 
@@ -493,26 +496,33 @@ function HomePage(options) {
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
 
+
+                {/* image */}
+            </div>
+
+        </section>
         <div className={cx("detail-image")}>
             <img src="./image/detail-homePage.jpg" alt="detail" />
 
         </div>
 
-        <div className={cx("sub-page")}>
-            <h3>"Yêu thương trao đi là yêu thương còn mãi"</h3>
-            <p>Hãy cùng chúng tôi chọn người -
-                chọn nơi để thương, để yêu, để sẻ chia.
-                Và đó là những em nhỏ vùng cao ngoan hiền,
-                đang sống ở những nơi nghèo khó, giúp các em bớt chật vật hơn
-                khi tới trường. Hãy cùng cảm nhận niềm vui,
-                hạnh phúc với các em bằng những đóng góp nho nhỏ -
-                ít thôi nhưng đều đặn. Yêu thương bao giờ cũng có
-                đủ cho tất cả mọi người. Sự yêu thương mà chúng ta cho
-                đi là sự yêu thương mà chúng ta giữ mãi được cho mình.
-            </p>
+
+        <div className="container">
+            <div className={cx("sub-detail-page")}>
+
+                <h3>"Yêu thương trao đi là yêu thương còn mãi"</h3>
+                <p>Hãy cùng chúng tôi chọn người -
+                    chọn nơi để thương, để yêu, để sẻ chia.
+                    Và đó là những em nhỏ vùng cao ngoan hiền,
+                    đang sống ở những nơi nghèo khó, giúp các em bớt chật vật hơn
+                    khi tới trường. Hãy cùng cảm nhận niềm vui,
+                    hạnh phúc với các em bằng những đóng góp nho nhỏ -
+                    ít thôi nhưng đều đặn. Yêu thương bao giờ cũng có
+                    đủ cho tất cả mọi người. Sự yêu thương mà chúng ta cho
+                    đi là sự yêu thương mà chúng ta giữ mãi được cho mình.
+                </p>
+            </div>
         </div>
 
         <div className={cx("section-fearute", "container-fluid")}>
@@ -543,8 +553,8 @@ function HomePage(options) {
                         <div className={cx("item-fear")}>
                             <h3>TỔ CHỨC</h3>
                             <p>
-                            Trụ sở chính tại Hà Nội, mạng lưới tình nguyện trên cả nước và nhiều quốc gia. Các hình thức ủng hộ quỹ gồm: Cộng đồng ủng hộ; Quỹ từ thiện độc lập là đối tác thường xuyên; Các cá nhân ủng hộ...Chủ tịch Hội đồng quản lý: Nhà báo Trần Đăng Tuấn; Chủ tịch danh dự: GS. Ngô Bảo Châu; Phó chủ tịch kiêm Giám đốc Quỹ điều hành công việc hàng ngày: Nhà báo Nguyễn Anh Tú.
-                               </p>
+                                Trụ sở chính tại Hà Nội, mạng lưới tình nguyện trên cả nước và nhiều quốc gia. Các hình thức ủng hộ quỹ gồm: Cộng đồng ủng hộ; Quỹ từ thiện độc lập là đối tác thường xuyên; Các cá nhân ủng hộ...Chủ tịch Hội đồng quản lý: Nhà báo Trần Đăng Tuấn; Chủ tịch danh dự: GS. Ngô Bảo Châu; Phó chủ tịch kiêm Giám đốc Quỹ điều hành công việc hàng ngày: Nhà báo Nguyễn Anh Tú.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -577,7 +587,6 @@ function HomePage(options) {
 
             </div>
         </div>
-        {/* <div className={cx("footer")}></div> */}
 
 
     </>
